b0796da9b8ed8a961136e642be5c50066ba4bd69
berkcinar <berkcinar01@gmail.com>
2021-04-03 21:49:21 +0300

    first commit for java

--- com/berk/Main.java
+++ com/berk/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World ***");
         System.out.println("Hello World ***");
-        System.out.println("Hello World ***");
 
 
     }
